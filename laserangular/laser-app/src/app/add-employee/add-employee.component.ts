a373853461159e7f1722ffe1cd5dde5606a4c052
Dawid Strozak <dawidst@oswald.local>
2017-08-17 10:41:16 +0100

    basic working page

---
+++ laserangular/laser-app/src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'laser-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
